5d0c28fa436c0212ccabfdc9c2b57d6220b395b0
Jeremy Schwartz <jeremys.atwork@gmail.com>
2016-10-22 08:29:48 -0400

    fix: php parsing issue (svn repo blocked)

--- test-xml-response.php
+++ test-xml-response.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?> <env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://tempuri.org/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?> <env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://tempuri.org/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 	<env:Body>
 		<ns1:Response>
 			<ns1:Resultstatus><?php echo $_GET['status'] ?></ns1:Resultstatus>
